b50bbe3a8fe462106b9d0ed58dce45a5208cab19
sbx_user1051 <sbx_user1051@169.254.97.155>
2025-07-02 16:26:15 +0000

    Wed Jul 02 16:26:15 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,11 @@
 
 class SumSolution:
     
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def compute(self, x: int, y: int) -> int:
+        """Add two numbers and return a total"""
+
+        if x > 100 or x < 0 or y > 100 or y < 0:
+            raise ValueError(f"Arguments must be between 0-100")
+
+        return x + y 
+
